e5a13eecec7634225ed01b5e38621693d054f923
Ryan Jones <ryancjonesy@gmail.com>
2026-03-07 14:25:04 -0600

    added while loop to loop through returned rows, extracted the date from the comment, formatted it for sql, and ran the query to update the row

--- updateShipDates.php
+++ updateShipDates.php
@@ -2,8 +2,24 @@
 require 'db.php';
 
 $sqlQuery =
-    "SELECT id, comments
+    "SELECT *
      FROM sweetwater_test
      WHERE shipdate_expected = '0000-00-00 00:00:00'";
 
 $queryResult = $dbConnection->query($sqlQuery);
+
+while ($currentRow = $queryResult->fetch_assoc()) {
+    $currentComment = $currentRow['comments'];
+    // only process rows that contain an expected ship date in the comment
+    if (str_contains(strtolower($currentComment), 'expected ship date')) {
+        //find the position of the colon in the comment and extract the date that follows it
+        $positionOfColon = strpos($currentComment, ':');
+        $extractDate = substr($currentComment, $positionOfColon + 1);
+        $date = strtotime($extractDate);
+        // format it for SQL
+        $formattedDate = date('Y-m-d', $date);
+        //run query to update the ship date for the current order
+        $dbConnection->query("UPDATE sweetwater_test SET shipdate_expected = '$formattedDate' WHERE orderid = '" . $currentRow['orderid'] . "'");
+    }
+}
+echo "Ship dates updated successfully.";
